3e8a78e4957a25e5f6d1beacad95d566827b928f
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2023-09-28 20:51:11 +0300

    test

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-         /* origin: "http://localhost:3000", */
-        origin: "https://tictactoe-s.herokuapp.com/",
+         origin: "http://localhost:3000",
+        /* origin: "https://tictactoe-s.herokuapp.com/" */
         methods: ["GET", "POST"],
     }
 })
